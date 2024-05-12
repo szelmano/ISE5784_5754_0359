6cdab4b61cfc58f2508cafe59809830711a46e66
Shirel Zelmanovich <szelmano@g.jct.ac.il>
2024-05-12 19:13:24 +0300

    stage 2 vector test

--- unittests/primitives/PointTests.java
+++ unittests/primitives/PointTests.java
@@ -3,28 +3,71 @@ package primitives;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PointTests {
 
+    private final Point p1 = new Point(1,2,3);
+    private final Point p2= new Point(7,6,5);
+    private final Vector v1= new Vector (-1,-2,-3);
+
+
+
+    /**
+     * checking if the adding work correctly
+     */
     @Test
     void testAdd() {
-        Point  point1 = new Point(1, 2, 3);
-        Point  point2 = new Point(4, 5, 6);
-        // Adding the vectors and getting the result
-        Point actual =  point1.add(point2);
-        // Checking that the result is correct
-        assertEquals(new Vector(5, 7, 9),actual,"ERROR: Vector + Vector does not work correctly");
+        // =======Equivalence Partitions Tests=======
+        // TC01: testing the adding of point and vector (simple test)
+        assertEquals(new Point(0, 0, 0),p1.add(v1), "the adding does not work correctly");
     }
 
+    /**
+     * checking if the subtracting work correctly
+     */
     @Test
     void testSubtract() {
+        // =======Equivalence Partitions Tests=======
+        // TC01: testing the subtract between two points (simple test)
+        assertEquals(new Vector(6, 4, 2),p2.subtract(p1),"ERROR: TC01");
+
+        // ======== subtraction of a point itself ========
+        assertThrows(IllegalArgumentException.class,
+                () -> p1.subtract(p1),
+                "the subtracting does not work correctly");
     }
 
+    /**
+     * checking if the DistanceSquared function work correctly
+     */
     @Test
     void testDistanceSquared() {
+        // =======Equivalence Partitions Tests=======
+        // TC01: testing if the distance squared between two points is correct (simple
+        // test)
+        assertEquals(56, p1.distanceSquared(p2), "ERROR: DistanceSquared doesn't work correctly");
+
+        // =============== Boundary Values Tests ==================
+
+        // TC11: testing if the distance squared between the point to itself is zero
+        assertEquals(0, p1.distanceSquared(p1), "ERROR: DistanceSquared doesn't work correctly");
     }
 
+    /**
+     * checking if the Distance function work correctly
+     */
     @Test
     void testDistance() {
+        // =======Equivalence Partitions Tests=======
+        // TC01: testing if the distance between two points -with squared - is correct
+        // (simple test)
+        assertEquals(Math.sqrt(56), p1.distance(p2), "ERROR: Distance doesn't work correctly");
+
+        // =============== Boundary Values Tests ==================
+
+        // TC11: testing if the distance between the point to itself is zero
+        assertEquals(0, p1.distance(p1), "ERROR: Distance doesn't work correctly");
+
     }
 }
--- unittests/primitives/VectorTests.java
+++ unittests/primitives/VectorTests.java
@@ -6,89 +6,101 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class VectorTests {
 
+    private Vector v1 = new Vector(1, 2, 3);
+    private Vector v2 = new Vector(2, 4, 6);
+    private Vector v3 = new Vector(1, 2, 4);
+    private Vector v4 = new Vector(1, 4, -3);
+    private Vector v5 = new Vector(-1, -2, -3);
+
+    /**
+     * The function checks the integri of the constructor if a test fails it will
+     * print a message
+     */
     @Test
-    void testAdd() {
-        // Creating two vectors for testing
-        Vector vector1 = new Vector(1, 2, 3);
-        Vector vector2 = new Vector(4, 5, 6);
-        // Adding the vectors and getting the result
-        Vector actual = vector1.add(vector2);
-        // Checking that the result is correct
-        assertEquals(new Vector(5, 7, 9),actual,"ERROR: Vector + Vector does not work correctly");
+    public void testConstructor() {
+        // ============ Equivalence Partitions Tests ==============
+        //TC01: only one regular test to check
+        assertDoesNotThrow(() -> new Vector(1,2,3), "Does not construct a Vector");
+
+
+        //=============== Boundary Values Tests ==================
+        // TC01: Constructor of (0,0,0) vector throws an exception
+        assertThrows(IllegalArgumentException.class, ()-> new Vector(0, 0, 0),
+                "Constructed (0,0,0) vector");
     }
 
+    /**
+     * The function checks the integrity of the dotProduct function if a test fails
+     * it will print a message
+     */
     @Test
-    void testScale() {
-        // Creating a vector for testing
-        Vector vector = new Vector(2, 2, 3);
+    void dotProduct() {
+        // =======Equivalence Partitions Tests=======
+        // TC:01 in the same direction
+        assertEquals(28, v1.dotProduct(v2), "ERROR: TC:01");
+        // TC:02 sharp angle
+        assertEquals(17, v1.dotProduct(v3), "ERROR:  TC:02");
+        // TC:03 Obtuse angle
+        assertEquals(-3, v3.dotProduct(v4), "ERROR: TC:03");
+        // TC:04 Inverted vector
+        assertEquals(-14, v1.dotProduct(v5), "ERROR: TC:04");
 
-        // Scaling the vector and getting the result
-        Vector actual = vector.scale(2);
-
-        // Checking that the result is correct
-        assertEquals(new Vector(4, 4, 6), actual,"ERROR: Vector * scalar does not work correctly");
+        // =============== Boundary Values Tests ==================
+        // TC:11 Orthogonal angle
+        assertEquals(0, v1.dotProduct(v4), "ERROR:  TC:11");
     }
 
+    /**
+     * The function checks the integrity of the crossProduct function if a test
+     * fails it will print a message
+     */
     @Test
-    void testDotProduct() {
-        // Creating two vectors for testing
-        Vector vector1 = new Vector(1, 2, 3);
-        Vector vector2 = new Vector(4, 5, 6);
-
-        // Computing the dot product of the vectors
-        double actual = vector1.dotProduct(vector2);
-
-        // Checking that the result is correct
-        assertEquals(32, actual,"ERROR: dotProduct() wrong value");
+    void crossProduct() {
+        // TC:01: sharp angle
+        assertEquals(new Vector(2, -1, 0), vec1.crossProduct(vec3), "ERROR: TC:01");
+        // TC:02 Orthogonal angle
+        assertEquals(new Vector(-18, 6, 2), vec1.crossProduct(vec4), "ERROR: TC:02");
+        // TC:03 Obtuse angle
+        assertEquals(new Vector(-22, 7, 2), vec3.crossProduct(vec4), "ERROR: TC:03");
+        // TC:04 Inverted vector
+        assertThrows(IllegalArgumentException.class, () -> vec1.crossProduct(vec5), "ERROR: TC:04");
+        // TC:05 Two vectors with the same direction
+        assertThrows(IllegalArgumentException.class, () -> vec1.crossProduct(vec2), "ERROR: TC:05");
     }
 
+    /**
+     * The function checks the integrity of the lengthSquared function if a test
+     * fails it will print a message
+     */
     @Test
-    void testCrossProduct() {
-        // Creating two vectors for testing
-        Vector vector1 = new Vector(1, 2, 3);
-        Vector vector2 = new Vector(4, 5, 6);
-
-        // Computing the cross product of the vectors and getting the result
-        Vector actual = vector1.crossProduct(vector2);
-
-        // Checking that the result is correct
-        assertEquals(new Vector(-3, 6, -3), actual,"ERROR: crossProduct() wrong value");
+    void lengthSquared() {
+        // TC:01 negative coordinate
+        assertEquals(14, vec5.lengthSquared(), "ERROR: TC:01");
+        // TC:02 positive coordinate
+        assertEquals(14, vec1.lengthSquared(), "ERROR: TC:02");
+        // TC:03 positive and negative coordinate
+        assertEquals(26, vec4.lengthSquared(), "ERROR: TC:03");
     }
 
+    /**
+     * The function checks the integrity of the length function if a test fails it
+     * will print a message
+     */
     @Test
-    void testLengthSquared() {
-        // Creating a vector for testing
-        Vector vector = new Vector(1, 2, 3);
-
-        // Computing the squared length of the vector
-        double actual = vector.lengthSquared();
-
-        // Checking that the result is correct
-        assertEquals(14, actual,"ERROR: lengthSquared() wrong value");
+    void length() {
+        // TC01: A test of the distance between two points in a 3D
+        assertEquals(Math.sqrt(26), vec4.length(), "ERROR: TC:01");
     }
 
+    /**
+     * The function checks the integrity of the normalize function if a test fails
+     * it will print a message
+     */
     @Test
-    void testLength() {
-        // Creating a vector for testing
-        Vector vector = new Vector(1, 2, 3);
+    void normalize() {
+        // TC:01: A test of the vector normalize in a 3D
+        assertEquals(new Vector(-1 / Math.sqrt(14), -2 / Math.sqrt(14), -3 / Math.sqrt(14)), vec5.normalize(),
+                "ERROR TC:01");
 
-        // Computing the length of the vector
-        double actual = vector.length();
-
-        // Checking that the result is correct
-        assertEquals(Math.sqrt(14), actual,"ERROR: length() wrong value");
-    }
-
-    @Test
-    void testNormalize() {
-        // Creating a vector for testing
-        Vector vector = new Vector(1, 2, 3);
-
-        // Normalizing the vector and getting the result
-        Vector actual = vector.normalize();
-
-        // Checking that the result is correct
-        assertEquals(new Vector(1 / Math.sqrt(14), 2 / Math.sqrt(14), 3 / Math.sqrt(14))
-                , actual,"ERROR: the normalized vector is not a unit vector");
     }
 }
